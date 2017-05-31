e1f6db480a31721b3ebbcf2f8b8f1fa3ed23867e
John Berroa <johnberroa@gmail.com>
2017-05-31 18:54:53 +0200

    Added PROC, still not connected to df yet

--- technical_analysis.py
+++ technical_analysis.py
@@ -12,8 +12,9 @@ def RSI(dataframe, period):
     # for symbol in dataframe:
     #     all_prices = dataframe[symbol].close
     # ---
+
         diff = np.diff(all_prices) # length is 1 less than the all_prices
-        rsi = []
+        rsi = [None] * period # because RSI can't be calculated until period prices have occured
 
         for i in range(len(diff) - period):
             avg_gain = diff[i:period + i]
@@ -28,9 +29,28 @@ def RSI(dataframe, period):
                 rs = avg_gain / avg_loss
                 rsi.append(100 - (100 / (1 + rs)))
 
-    return rsi
+        return rsi
+
+
+def PROC(dataframe, period):
+    '''
+    Computes the PROC(price rate of change) of a given price series for a given period length
+    :param dataframe:
+    :param period:
+    :return proc:
+    '''
+
+    # TODO:---
+    # for symbol in dataframe:
+    #     all_prices = dataframe[symbol].close
+    # ---
+        proc = [None] * period  # because PROC can't be calculated until period prices have occured
+        for i in range(len(all_prices) - period):
+            proc.append((all_prices[i + period] - all_prices[i]) / all_prices[i])
+
+    return proc
 
 # print(RSI([1,2,3,4,5,4,3,2,1], 2))
-print(RSI([1.00,2.00,3.00,4.00,5.00,4.00,3.00,2.00,1.00,1.00,2.00,3.00,
-     4.00,5.00,4.00,3.00,2.00,1.00,1.00,2.00,3.00,4.00,5.00,4.00,
-     3.00,2.00,1.00,2.00,3.00,4.00,5.00,4.00], 14))
+# print(RSI([1.00,2.00,3.00,4.00,5.00,4.00,3.00,2.00,1.00,1.00,2.00,3.00,
+#      4.00,5.00,4.00,3.00,2.00,1.00,1.00,2.00,3.00,4.00,5.00,4.00,
+#      3.00,2.00,1.00,2.00,3.00,4.00,5.00,4.00], 14))
