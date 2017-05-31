383a013314116e76705ceca08fddfb85adc9686b
John Berroa <johnberroa@gmail.com>
2017-05-31 18:28:16 +0200

    Added RSI, currently doesn't load in dataframe

---
+++ technical_analysis.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def RSI(dataframe, period):
+    '''
+    Computes the RSI of a given price series for a given period length
+    :param dataframe:
+    :param period:
+    :return rsi:
+    '''
+
+    # TODO:---
+    # for symbol in dataframe:
+    #     all_prices = dataframe[symbol].close
+    # ---
+        diff = np.diff(all_prices) # length is 1 less than the all_prices
+        rsi = []
+
+        for i in range(len(diff) - period):
+            avg_gain = diff[i:period + i]
+            avg_loss = diff[i:period + i]
+            avg_gain = abs(sum(avg_gain[avg_gain >= 0]) / period)
+            avg_loss = abs(sum(avg_loss[avg_loss < 0]) / period)
+            if avg_loss == 0:
+                rsi.append(100)
+            elif avg_gain == 0:
+                rsi.append(0)
+            else:
+                rs = avg_gain / avg_loss
+                rsi.append(100 - (100 / (1 + rs)))
+
+    return rsi
+
+# print(RSI([1,2,3,4,5,4,3,2,1], 2))
+print(RSI([1.00,2.00,3.00,4.00,5.00,4.00,3.00,2.00,1.00,1.00,2.00,3.00,
+     4.00,5.00,4.00,3.00,2.00,1.00,1.00,2.00,3.00,4.00,5.00,4.00,
+     3.00,2.00,1.00,2.00,3.00,4.00,5.00,4.00], 14))
