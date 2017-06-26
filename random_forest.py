e998e68590991d856e71948eea0542c6238400d0
tpetri <tobias.petri94@gmx.de>
2017-06-26 12:43:26 +0200

    Random Forest now does stuff
    I redid the random forest a bit. Now takes X_Train Data and Y_Train
    Data. Also seems to work, now we just need to change that for all of our
    data.

--- random_forest.py
+++ random_forest.py
@@ -8,6 +8,7 @@ http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestCl
 
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier as make_forest
+import numpy as np
 
 '''
 ### Outline ###
@@ -17,16 +18,33 @@ We then input the data and features in to the first .fit parameter, and the labe
 '''
 
 num_features = 4
-prediction_window = 30
+prediction_window = 10
 full_data = pd.read_csv('data_preprocessed.csv')
+
 
 # drop all target columns not to be analyzed
 headers = full_data.columns.values
-headers = headers[10:] # should return just the headers of the DI-# (target values)
+headers = headers[12:] # should return just the headers of the DI-# (target values) FIXED
 headers = headers[headers!='DI-'+str(prediction_window)]
 selected_data = full_data.drop(headers, axis=1) # dunno if axis 1 is right
 selected_data = selected_data.dropna(axis=0, how='any') # using the subset parameter might allow us to skip dropping other targets?
 
+selected_data.drop(["Unnamed: 0"], axis = 1, inplace = True)
+selected_data.drop(["Date"], axis = 1, inplace = True)
+selected_data.drop(["Symbol"], axis = 1, inplace = True)
+
+# Split in Test-x and Test-y
+data_y = selected_data['DI-'+str(prediction_window)].as_matrix()
+#print(data_y)
+selected_data.drop(['DI-'+str(prediction_window)], axis = 1, inplace = True)
+data_x = selected_data.as_matrix()
+#print(data_x)
+
+# Ignore the warning. Works anyway
 Random_Forest = make_forest(max_features=num_features, bootstrap=True, oob_score=True, verbose=1)
 
-Random_Forest.fit(!!prices&features!!, selected_data['DI-'+str(prediction_window)])
+Random_Forest.fit(data_x, data_y)
+
+# Test using 1D array, should be 2d but yeah. Works. Just for the sake of it
+print("TEST: TREE VS ORIGINAL")
+print(str(Random_Forest.predict(data_x[10])) + " vs. " + str(data_y[10]))
