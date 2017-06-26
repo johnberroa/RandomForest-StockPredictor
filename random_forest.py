d051e4523dc88576cd9e717e30644977b8ef8c75
tpetri <tobias.petri94@gmx.de>
2017-06-26 14:10:36 +0200

    Cleaned up the code, nicer output

--- random_forest.py
+++ random_forest.py
@@ -8,6 +8,8 @@ http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestCl
 
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier as make_forest
+from sklearn.metrics import mean_squared_error as mse
+from sklearn.metrics import accuracy_score as acc
 import numpy as np
 
 '''
@@ -29,30 +31,23 @@ headers = headers[headers!='DI-'+str(prediction_window)]
 selected_data = full_data.drop(headers, axis=1) # dunno if axis 1 is right
 selected_data = selected_data.dropna(axis=0, how='any') # using the subset parameter might allow us to skip dropping other targets?
 
-### SPLIT IN TEST & TRAIN ###
+### Drop useless labels ###
 selected_data.drop(["Unnamed: 0"], axis = 1, inplace = True)
 selected_data.drop(["Date"], axis = 1, inplace = True)
 selected_data.drop(["Symbol"], axis = 1, inplace = True)
 
-
-# Split in Test-x and Test-y
+# Split in Test-x and Test-y #
 train_y = selected_data['DI-'+str(prediction_window)][:2000].as_matrix()
 test_y = selected_data['DI-'+str(prediction_window)][2000:2100].as_matrix()
-#print(data_y)
+
 selected_data.drop(['DI-'+str(prediction_window)], axis = 1, inplace = True)
-#print(selected_data)
 train_x = selected_data[:2000].as_matrix()
 test_x = selected_data[2000:2100].as_matrix()
 
-
-
 # Ignore the warning. Works anyway
 Random_Forest = make_forest(max_features=num_features, bootstrap=True, oob_score=True, verbose=1)
-
 Random_Forest.fit(train_x, train_y)
 
-# Test using 1D array, should be 2d but yeah. Works. Just for the sake of it
 print("TEST: TREE VS ORIGINAL")
 res = Random_Forest.predict(test_x)
-for i in range(len(res)):
-    print(str(res[i]) + " VS "+ str(test_y[i]))
+print("ACCURACY: "+str(acc(test_y, res)))
